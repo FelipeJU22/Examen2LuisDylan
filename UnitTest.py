94e80c70da185bf7e32c2d42e8e3cbd0e701e6ee
Dylan Garbanzo <dgarbanzof@estudiantec.cr>
2023-11-11 18:52:27 -0600

    Adición pruebas extra
    Se agregaron una prueba unitaria extra por cada feature

--- UnitTest.py
+++ UnitTest.py
@@ -8,15 +8,19 @@ class TestMiClase(unittest.TestCase):
 
     def test_ObtieneValencia(self):
         self.assertEqual(self.mi_clase_obj.ObtieneValencia(1234567), 4)
+        self.assertEqual(self.mi_clase_obj.ObtieneValencia(24680), 0)
 
     def test_DivisibleTempo(self):
         self.assertEqual(self.mi_clase_obj.DivisibleTempo(10), [1, 2, 5, 10])
+        self.assertEqual(self.mi_clase_obj.DivisibleTempo(7), [1, 7])
 
     def test_ObtieneMasBailable(self):
         self.assertEqual(self.mi_clase_obj.ObtieneMasBailable([0.8, 0.9, 0.7]), 0.9)
+        self.assertIsNone(self.mi_clase_obj.ObtieneMasBailable([]))
 
     def test_VerificaListaCanciones(self):
         self.assertTrue(self.mi_clase_obj.VerificaListaCanciones(["Canción 1", "Canción 2", "Canción 3"]))
+        self.assertFalse(self.mi_clase_obj.VerificaListaCanciones(["Canción 1", None, "Canción 3"]))
 
 if __name__ == '__main__':
     unittest.main()
