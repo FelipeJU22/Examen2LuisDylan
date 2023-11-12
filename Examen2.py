78033dddd3c6fd913f0c170826d9dedfc41bb10d
FelipeJU22 <felipejimenezu03@gmail.com>
2023-11-11 18:59:43 -0600

    Se actualiza Examen2.py
    Se agrega nueva función

--- Examen2.py
+++ Examen2.py
@@ -25,6 +25,12 @@ class MiClase:
         if any(song is None for song in lista):
             return False
         return True
+    def Encuentra(self, lista, elemento):
+        if elemento in lista:
+            return True
+        else:
+            return False
+    
 ################################################################################################
 #Ejemplo de ejecución
 # Crear un objeto de la clase MiClase
@@ -34,3 +40,4 @@ print(objeto.ObtieneValencia(1234567))  # Debería imprimir 4
 print(objeto.DivisibleTempo(10))  # Debería imprimir [1, 2, 5, 10]
 print(objeto.ObtieneMasBailable([0.8, 0.9, 0.7]))  # Debería imprimir 0.9
 print(objeto.VerificaListaCanciones(["Canción 1", "Canción 2", "Canción 3"]))  # Debería imprimir True
+print(objeto.Encuentra([1,2,3,4,5], 5))  # Debería imprimir True
